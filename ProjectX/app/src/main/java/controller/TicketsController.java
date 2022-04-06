737538bfc8b64730725a68eacc5dd0c9279b0b57
LukasGreven <70885688+LukasGreven@users.noreply.github.com>
2022-04-06 15:53:05 +0200

    package for controller added

---
+++ ProjectX/app/src/main/java/controller/TicketsController.java
@@ -0,0 +1,28 @@
+package controller;
+
+import android.content.Context;
+import android.util.AttributeSet;
+import android.view.View;
+
+import androidx.annotation.Nullable;
+
+public class TicketsController extends View {
+    public TicketsController(Context context) {
+        super(context);
+        init();
+    }
+
+    public TicketsController(Context context, @Nullable AttributeSet attrs) {
+        super(context, attrs);
+        init();
+    }
+
+    public TicketsController(Context context, @Nullable AttributeSet attrs, int defStyleAttr) {
+        super(context, attrs, defStyleAttr);
+        init();
+    }
+
+    private void init(){
+
+    }
+}
