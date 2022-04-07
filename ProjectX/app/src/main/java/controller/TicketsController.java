1bfe3a44d6a31b66a6be9414df380bf5c08f6a73
LukasGreven <70885688+LukasGreven@users.noreply.github.com>
2022-04-07 10:35:58 +0200

    hab was gemacht

--- ProjectX/app/src/main/java/controller/TicketsController.java
+++ ProjectX/app/src/main/java/controller/TicketsController.java
@@ -1,28 +1,35 @@
 package controller;
 
 import android.content.Context;
+import android.os.Bundle;
 import android.util.AttributeSet;
 import android.view.View;
+import android.widget.Button;
+import android.widget.LinearLayout;
+import android.widget.ScrollView;
 
 import androidx.annotation.Nullable;
+import androidx.appcompat.app.AppCompatActivity;
 
-public class TicketsController extends View {
-    public TicketsController(Context context) {
-        super(context);
-        init();
-    }
+import org.die6sheeshs.projectx.R;
 
-    public TicketsController(Context context, @Nullable AttributeSet attrs) {
-        super(context, attrs);
-        init();
-    }
-
-    public TicketsController(Context context, @Nullable AttributeSet attrs, int defStyleAttr) {
-        super(context, attrs, defStyleAttr);
+public class TicketsController extends AppCompatActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.tickets);
         init();
     }
 
     private void init(){
+        ScrollView scrollView = findViewById(R.id.scroll);
+        LinearLayout linearLayout = findViewById(R.id.linlay);
+        Button btnTag = new Button(this);
+        btnTag.setLayoutParams(linearLayout.getLayoutParams());
+        btnTag.setText("Button");
 
+
+        //add button to the layout
+        linearLayout.addView(btnTag);
     }
 }
