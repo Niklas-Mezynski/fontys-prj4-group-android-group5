60617073e8439645773ea383116d0956107b6f2a
Fabian Kerzmann <453734@student.fontys.nl>
2022-05-17 12:35:36 +0200

    Adds Friend Deletion endpoint

--- ProjectX/app/src/main/java/org/die6sheeshs/projectx/restAPI/FriendsApi.java
+++ ProjectX/app/src/main/java/org/die6sheeshs/projectx/restAPI/FriendsApi.java
@@ -5,6 +5,7 @@ import org.die6sheeshs.projectx.entities.Friend;
 import java.util.List;
 
 import io.reactivex.Observable;
+import retrofit2.http.DELETE;
 import retrofit2.http.GET;
 import retrofit2.http.Path;
 
@@ -12,5 +13,8 @@ public interface FriendsApi {
 
     @GET("/users/{id}/friends")
     public Observable<List<Friend>> getFriendsOfUser(@Path("id") String id);
+
+    @DELETE("/users/{user_id}/friends/{friend_id}")
+    public Observable<Void> deleteFriend(@Path("user_id") String userId, @Path("friend_id") String friendId);
 
 }
